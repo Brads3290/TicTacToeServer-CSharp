5afa87c02e1956780437e8cfb9de166660b36aad
Brad <solutioneers-slack@qweasdie.com>
2024-02-02 18:42:39 +1100

    Add constructor to GamePlayerInfo

--- src/TicTacToe.Api/Game/Models/GamePlayerInfo.cs
+++ src/TicTacToe.Api/Game/Models/GamePlayerInfo.cs
@@ -3,7 +3,11 @@ namespace TicTacToe.Api.Game.Models;
 public class GamePlayerInfo {
 
     public string PlayerId { get; set; }
-
     public string Symbol { get; set; }
+
+    public GamePlayerInfo(string playerId, string symbol) {
+        PlayerId = playerId;
+        Symbol = symbol;
+    }
 
 }
