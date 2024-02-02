ebbf0ee552f8d1c2c1872f885ad7bd2242ded09a
Brad Sullivan <brad@sullivandigital.com.au>
2024-02-02 17:55:24 +1100

    Fix namespaces

--- src/TicTacToe.Api/Players/PlayersController.cs
+++ src/TicTacToe.Api/Players/PlayersController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace TicTacToe.Api.Players.Controllers;
+namespace TicTacToe.Api.Players;
 
 [ApiController]
 public class PlayersController : ControllerBase {
--- src/TicTacToe.Api/Program.cs
+++ src/TicTacToe.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using TicTacToe.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -36,8 +38,12 @@ app.MapGet("/weatherforecast", () => {
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+namespace TicTacToe.Api {
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    }
 
 }
