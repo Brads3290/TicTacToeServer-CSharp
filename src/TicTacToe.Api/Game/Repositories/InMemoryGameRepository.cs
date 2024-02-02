c85d78e146de979d19c2afb567f90f9baeac23de
Onur Çağan <onurcagann@gmail.com>
2024-02-02 13:37:35 +0300

    Add InMemoryGameRepo

---
+++ src/TicTacToe.Api/Game/Repositories/InMemoryGameRepository.cs
@@ -0,0 +1,31 @@
+using TicTacToe.Api.Game.Interfaces;
+using TicTacToe.Api.Game.Models;
+
+public class InMemoryGameRepository : IGameRepository
+{
+    private readonly Dictionary<string, Game> _games = new();
+    public Task DeleteGame(Game game)
+    {
+        _games.Remove(game.Id);
+        return Task.CompletedTask;
+    }
+
+    public Task<Game?> GetGameAsync(string gameId)
+    {
+        _games.TryGetValue(gameId, out var game);
+
+        return Task.FromResult(game);
+
+    }
+
+    public Task<List<Game>> ListGameStatesAsync()
+    {
+        return Task.FromResult(_games.Values.ToList());
+    }
+
+    public Task SaveGameAsync(Game game)
+    {
+        _games[game.Id] = game;
+        return Task.CompletedTask;
+    }
+}
